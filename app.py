edcdd4c557dfac649289c65ecc2e60b8d62d9f2f
Kunal Mehta <kunmehta0@publicisgroupe.net>
2023-03-10 14:34:02 +0530

    Second Version of Link Directory
    Signed-off-by: Kunal Mehta <kunmehta0@publicisgroupe.net>

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -12,6 +12,7 @@ db.init_app(app)
 class link_class(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
+    category = db.Column(db.String, nullable = False)
     link = db.Column(db.String, nullable = False)
     link_dt = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
 
@@ -26,8 +27,11 @@ def home():
 
     if request.method == 'POST':
         link_title = request.form['title']
+        link_category = request.form['category']
+
         link_link = request.form['link']
-        link_obj = link_class(title = link_title, link = link_link)
+
+        link_obj = link_class(title = link_title, link = link_link, category = link_category)
         db.session.add(link_obj)
         db.session.commit()
 
@@ -43,6 +47,80 @@ def home():
 def about():
     return '<p>This is an About Page </p>'
 
+@app.route("/delete/<int:id>")
+def delete(id):
+    print(id)
+
+
+    link_del = db.get_or_404(link_class, id)
+    db.session.delete(link_del)
+    db.session.commit()
+
+    return redirect ('/')
+
+
+@app.route("/update/<int:id>", methods = ['GET', 'POST'])
+def update(id):
+    if request.method == 'POST':
+
+        link_update = db.get_or_404(link_class, id)
+        link_update.title = request.form['title']
+        link_update.category = request.form['category']
+
+        link_update.link = request.form['link']
+
+        db.session.commit()
+        return redirect('/')
+
+
+    print(id)
+    link_update = db.get_or_404(link_class, id)
+    return render_template('update.html', link_update = link_update)
+
+@app.route("/gettitle/<string:strtitle>")
+def gettitle(strtitle):
+    #print(strtitle)
+
+    get_title = link_class.query.filter_by(title = strtitle).all()
+    if len(get_title) == 0:
+        return "No Information Exist"
+    else: 
+    
+        for i in range(len(get_title)):
+            title = get_title[i].title
+            category = get_title[i].category
+            link = get_title[i].link
+
+            result = {
+                'Title': title, 
+                'Category': category,
+                'Link': link
+            }
+            return jsonify(result)
+        
+
+@app.route("/getcategory/<string:strtitle>")
+def getcategory(strtitle):
+    #print(strtitle)
+    result_list = []
+    get_category = link_class.query.filter_by(category = strtitle).all()
+    if len(get_category) == 0:
+        return "No Information Exist"
+    else: 
+    
+        for i in range(len(get_category)):
+            title = get_category[i].title
+            category = get_category[i].category
+            link = get_category[i].link
+
+            result = {
+                'Title': title, 
+                'Category': category,
+                'Link': link
+            }
+            result_list.append(result)
+        return jsonify(result_list)
+    #return "this is response"
 
 if __name__ == '__main__':
     app.run(debug=True)
